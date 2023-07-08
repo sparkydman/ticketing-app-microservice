934a0a13a6dd6de4fdbcf10c7dcafd5ef5fdbe0f
Chigozie Ugwuede <sparkyglobe@gmail.com>
2023-07-09 00:22:44 +0100

    added more field

--- auth/src/schema/signup-schema.ts
+++ auth/src/schema/signup-schema.ts
@@ -1,15 +1,25 @@
 import { object, string, TypeOf } from 'zod';
+import _ from 'lodash';
 
 export const SignUpRequest = object({
   body: object({
     email: string({ required_error: 'Email is required' }).email(
       'Invalid email address'
     ),
-    username: string({ required_error: 'Username is required' }).min(
-      4,
-      'Username must be at least 4 characters'
+    firstname: string({ required_error: 'firstname is required' }),
+    lastname: string({ required_error: 'lastname is required' }),
+    password: string({ required_error: 'password is required' }).min(
+      6,
+      'password must be at least 6 characters'
     ),
+    confirmPassword: string({ required_error: 'confirmPassword is required' }),
+  }).refine((d) => d.password === d.confirmPassword, {
+    message: 'password dont match',
+    path: ['confirmPassword'],
   }),
 });
 
-export type SignUpType = TypeOf<typeof SignUpRequest>;
+export type SignUpType = Omit<
+  TypeOf<typeof SignUpRequest>,
+  'body.confirmPassword'
+>;
